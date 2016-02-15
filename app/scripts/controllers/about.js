f0bf3b9697685934231aee6d947e4e41f1c6aec4
Oisín Mac Fhearaí <denpashogai@gmail.com>
2016-02-15 23:45:36 +0000

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoKittyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yoKittyApp
+ */
+angular.module('yoKittyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoKittyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoKittyApp
+ */
+angular.module('yoKittyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
